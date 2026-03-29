752c16f49fe0edd6c5865229a35013a8d5b74705
subekshyakharel <subekshyakharel922@gmail.com>
2026-03-29 06:30:42 +0545

    small changes

--- lib/localStorage.ts
+++ lib/localStorage.ts
@@ -1,9 +1,9 @@
 import { Post } from "@/types/post";
 
-// 🔹 create unique key per user
+//  create unique key per user
 const getStorageKey = (userId: number) => `local_posts_user_${userId}`;
 
-// 🔹 get posts from localStorage
+// get posts from localStorage
 export const getLocalPostsByUser = (userId: number): Post[] => {
   if (typeof window === "undefined") return [];
 
@@ -11,7 +11,7 @@ export const getLocalPostsByUser = (userId: number): Post[] => {
   return data ? JSON.parse(data) : [];
 };
 
-// 🔹 save posts to localStorage
+// save posts to localStorage
 export const saveLocalPostsByUser = (userId: number, posts: Post[]) => {
   if (typeof window === "undefined") return;
 
